e121d64cae497b4c05f99268bbbf214de1a484a8
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-02 13:26:01 +0530

    Beautification

--- components/simple_header_component_1722585157.js
+++ components/simple_header_component_1722585157.js
@@ -4,14 +4,14 @@
 Vue.component("simple_header_component_1722585157", {
     template: `
     <header id="header-section" class="w-full">
-        <nav id="nav-bar" class="bg-pink-400 bg-opacity-20 backdrop-filter backdrop-blur-lg border-purple-300 border-opacity-30 py-4 dark:bg-purple-800 dark:bg-opacity-20">
+        <nav id="nav-bar" class="bg-pink-400 bg-opacity-20 backdrop-filter backdrop-blur-lg border-purple-300 border-opacity-30 py-4 dark:bg-purple-800 dark:bg-opacity-20 rounded-lg shadow-lg">
             <div id="nav-container" class="flex flex-wrap items-center justify-between max-w-screen-xl px-6 mx-auto">
                 <a id="logo-link" href="#" class="flex items-center">
-                    <img id="logo-image" src="./images/logo.svg" class="h-8 mr-4 sm:h-10" alt="Landwind Logo" />
-                    <span id="app-name" class="self-center text-2xl font-bold whitespace-nowrap text-purple-800 dark:text-pink-300">DecryptData - Blockchain Analytics Platform</span>
+                    <img id="logo-image" src="./images/logo.svg" class="h-10 mr-4 sm:h-12" alt="Landwind Logo" />
+                    <span id="app-name" class="self-center text-2xl font-bold whitespace-nowrap text-pink-600 dark:text-pink-300">DecryptData - Blockchain Analytics Platform</span>
                 </a>
                 <div id="menu-toggle-button" class="flex items-center lg:order-2">
-                    <button id="collapse-button" data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-purple-700 rounded-lg lg:hidden hover:bg-pink-200 hover:bg-opacity-30 focus:outline-none focus:ring-2 focus:ring-pink-400 dark:text-pink-300 dark:hover:bg-purple-600 dark:hover:bg-opacity-30 dark:focus:ring-pink-500" aria-controls="mobile-menu-2" aria-expanded="false">
+                    <button id="collapse-button" data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-pink-600 rounded-lg lg:hidden hover:bg-pink-200 hover:bg-opacity-30 focus:outline-none focus:ring-2 focus:ring-pink-400 dark:text-pink-300 dark:hover:bg-purple-600 dark:hover:bg-opacity-30 dark:focus:ring-pink-500" aria-controls="mobile-menu-2" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg id="menu-open-icon" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
@@ -23,10 +23,10 @@ Vue.component("simple_header_component_1722585157", {
                 </div>
                 <div id="mobile-menu-2" class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
                     <ul id="nav-list" class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                        <li id="nav-item-home"><a href="#" class="block py-2 pl-3 pr-4 text-purple-800 bg-pink-300 bg-opacity-30 rounded-lg lg:bg-transparent lg:text-purple-800 lg:p-0 dark:text-pink-300" aria-current="page">Real-time Monitoring</a></li>
-                        <li id="nav-item-1"><a href="#" class="block py-2 pl-3 pr-4 text-purple-800 border-b border-purple-200 hover:bg-pink-200 hover:bg-opacity-30 lg:hover:bg-transparent lg:border-0 lg:hover:text-pink-500 lg:p-0 dark:text-pink-300 lg:dark:hover:text-white dark:hover:bg-purple-600 dark:hover:bg-opacity-30 dark:border-purple-700">On-chain Data Analysis</a></li>
-                        <li id="nav-item-2"><a href="#" class="block py-2 pl-3 pr-4 text-purple-800 border-b border-purple-200 hover:bg-pink-200 hover:bg-opacity-30 lg:hover:bg-transparent lg:border-0 lg:hover:text-pink-500 lg:p-0 dark:text-pink-300 lg:dark:hover:text-white dark:hover:bg-purple-600 dark:hover:bg-opacity-30 dark:border-purple-700">Smart Contract Auditing</a></li>
-                        <li id="nav-item-3"><a href="#" class="block py-2 pl-3 pr-4 text-purple-800 border-b border-purple-200 hover:bg-pink-200 hover:bg-opacity-30 lg:hover:bg-transparent lg:border-0 lg:hover:text-pink-500 lg:p-0 dark:text-pink-300 lg:dark:hover:text-white dark:hover:bg-purple-600 dark:hover:bg-opacity-30 dark:border-purple-700">Transaction Tracking</a></li>
+                        <li id="nav-item-home"><a href="#" class="block py-2 pl-3 pr-4 text-pink-600 bg-pink-300 bg-opacity-30 rounded-lg lg:bg-transparent lg:text-pink-600 lg:p-0 dark:text-pink-300 hover:bg-pink-200 hover:bg-opacity-30 transition duration-300" aria-current="page">Real-time Monitoring</a></li>
+                        <li id="nav-item-1"><a href="#" class="block py-2 pl-3 pr-4 text-pink-600 border-b border-purple-200 hover:bg-pink-200 hover:bg-opacity-30 lg:hover:bg-transparent lg:border-0 lg:hover:text-pink-500 lg:p-0 dark:text-pink-300 lg:dark:hover:text-white dark:hover:bg-purple-600 dark:hover:bg-opacity-30 dark:border-purple-700 transition duration-300">On-chain Data Analysis</a></li>
+                        <li id="nav-item-2"><a href="#" class="block py-2 pl-3 pr-4 text-pink-600 border-b border-purple-200 hover:bg-pink-200 hover:bg-opacity-30 lg:hover:bg-transparent lg:border-0 lg:hover:text-pink-500 lg:p-0 dark:text-pink-300 lg:dark:hover:text-white dark:hover:bg-purple-600 dark:hover:bg-opacity-30 dark:border-purple-700 transition duration-300">Smart Contract Auditing</a></li>
+                        <li id="nav-item-3"><a href="#" class="block py-2 pl-3 pr-4 text-pink-600 border-b border-purple-200 hover:bg-pink-200 hover:bg-opacity-30 lg:hover:bg-transparent lg:border-0 lg:hover:text-pink-500 lg:p-0 dark:text-pink-300 lg:dark:hover:text-white dark:hover:bg-purple-600 dark:hover:bg-opacity-30 dark:border-purple-700 transition duration-300">Transaction Tracking</a></li>
                     </ul>
                 </div>
             </div>
